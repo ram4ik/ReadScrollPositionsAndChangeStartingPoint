acaa8bc00f6221ce9a10ed4950c03ceda1a08a14
ram4ik <ram4ik@hotmail.com>
2024-06-26 20:17:37 +0300

    Initial Commit

---
+++ ReadScrollPositionsAndChangeStartingPoint/ReadScrollPositionsAndChangeStartingPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadScrollPositionsAndChangeStartingPointApp.swift
+//  ReadScrollPositionsAndChangeStartingPoint
+//
+//  Created by Ramill Ibragimov on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadScrollPositionsAndChangeStartingPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReadScrollPositionsAndChangeStartingPointTests/ReadScrollPositionsAndChangeStartingPointTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadScrollPositionsAndChangeStartingPointTests.swift
+//  ReadScrollPositionsAndChangeStartingPointTests
+//
+//  Created by Ramill Ibragimov on 26.06.2024.
+//
+
+import Testing
+@testable import ReadScrollPositionsAndChangeStartingPoint
+
+struct ReadScrollPositionsAndChangeStartingPointTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
